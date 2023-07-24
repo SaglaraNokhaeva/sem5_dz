28c7cea037b2a3997671318b740b163d8124b288
Saglara <nohaevass@yandex.ru>
2023-07-24 23:43:32 +0300

    1

--- ex2.py
+++ ex2.py
@@ -1,21 +1,17 @@
-# ✔ Напишите однострочный генератор словаря, который принимает
-# на вход три списка одинаковой длины: имена str, ставка int,
-# премия str с указанием процентов вида «10.25%». В результате
-# получаем словарь с именем в качестве ключа и суммой
-# премии в качестве значения. Сумма рассчитывается
-# как ставка умноженная на процент премии
+# ✔ Напишите однострочный генератор словаря, который принимает на вход три списка одинаковой длины:
+# имена str, ставка int, премия str с указанием процентов вида «10.25%».
+# В результате получаем словарь с именем в качестве ключа и суммой премии в качестве значения.
+# Сумма рассчитывается как ставка умноженная на процент премии
+#
+from pprint import pprint
+# def grants_dict (names, pays, persents):
+#     persents = list(map(lambda x: float(x[:-1])/100, persents))
+#     return {name: pay*persent for name, pay, persent in zip(names, pays, persents)}
+#
+names = ["Иван", "Петр", "Михаил"]
+pays = [10000, 15000, 20000]
+persents = ['50.25%', '20%', '30.6%']
+# pprint(grants_dict(names, pays, persents))
 
-
-# link ='C:\Thecode\Media\статья.txt'
-# p, s = link.split('.',1)
-# print(p)
-# print(s)
-link ='C:\Thecode\Media\статья.txt'
-a, _, c  = link.rpartition('\\')
-print(a)
-print(c)
-
-a = 'Первый - второй - третий'
-# Разбиваем строку по самому правому ' - ', берем левую часть
-b = a.rpartition(' - ')
-print(b)  # Первый - второй
+my_dictcomp = {i: j*k for (i, j, k) in zip(names, pays, map(lambda x: float(x[:-1])/100, persents)) }
+pprint(my_dictcomp)
