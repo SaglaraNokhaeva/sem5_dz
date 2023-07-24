ee061fa5525811d7008afd1f59f5c8c5da937ce2
Saglara <nohaevass@yandex.ru>
2023-07-25 00:01:09 +0300

    1

--- ex3.py
+++ ex3.py
@@ -1,2 +1,17 @@
 # ✔ Создайте функцию генератор чисел Фибоначчи (см. Википедию).
 
+count = int(input('Введите длину ряда Фибоначчи: '))
+
+def fib(n):
+    if n == 1:
+        print('1')
+    elif n == 2:
+        print('1 1', end=' ')
+    else:
+        f1 = f2 = 1
+        print('1 1', end=' ')
+        for i in range(2, n):
+            f1, f2 = f2, f1 + f2
+            yield f2
+
+print(*fib(count))
